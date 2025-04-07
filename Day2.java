ceb8f96138ab77181fed2ab4fcd84aa9be1140f0
SahilAglawe <sahilaglawe66@gmail.com>
2025-04-07 16:47:28 +0530

    Add solution to push all zeroes to the end of an array

---
+++ Day2.java
@@ -0,0 +1,17 @@
+// Question : push all zeroes to end of array
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int n = arr.length;
+        int j = 0;
+        for(int i = 0; i<n; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+}
