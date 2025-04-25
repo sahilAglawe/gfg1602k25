60f69cefda506d4d588d4db2aeb3d8a2501efbc1
SahilAglawe <sahilaglawe66@gmail.com>
2025-04-25 10:36:03 +0530

    Add implementation to check if two strings are rotations of each other

--- Day20.java
+++ Day20.java
@@ -2,7 +2,7 @@
 
 
 
-class Solution {
+class Day20 {
     // Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2) {
         int n1 = s1.length();
