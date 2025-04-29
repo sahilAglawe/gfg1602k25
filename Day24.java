8243bd72fa65f70d7e3fe9de23fe95fbc1824531
SahilAglawe <sahilaglawe66@gmail.com>
2025-04-29 08:56:41 +0530

    Add implementation to merge overlapping intervals

---
+++ Day24.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<int[]> mergeOverlap(int[][] arr) {
+        int n = arr.length;
+        
+        Arrays.sort(arr, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0] - b[0];
+            }
+        });
+        
+        List<int[]> ans = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            if(ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1)[1]){
+                ans.add(new int[]{arr[i][0], arr[i][1]});
+            } else {
+                ans.get(ans.size() - 1)[1] = 
+                Math.max(ans.get(ans.size() - 1)[1], arr[i][1]);
+            }
+        }
+        return ans;
+    }
+}
+
+// T/C: O(nlogn) + O(n) = O(nLogn)
+// S/C: O(n)
