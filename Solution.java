a6bff095c3aed651c6f0fe92d39369f5a19ed4f3
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-03 08:11:35 +0530

    Add implementation for merging two sorted arrays

---
+++ Solution.java
@@ -0,0 +1,29 @@
+// Question : Merged two sorted arrays
+
+
+
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        int n = a.length;
+        int m = b.length;
+        
+        int left = n-1;
+        int right = 0;
+        
+        while(left >= 0 && right < m) {
+            if(a[left] > b[right]){
+                int temp = a[left];
+                a[left] = b[right];
+                b[right] = temp;
+            }
+            else break;
+            left--;
+            right++;
+            
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        
+    }
+}
