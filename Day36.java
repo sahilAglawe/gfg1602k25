9119c326c6596ad4f6dd7cd6c58c4cebef1af275
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-12 11:42:56 +0530

    Add implementation for spirally traversing a matrix

---
+++ Day36.java
@@ -0,0 +1,38 @@
+// Question : Spirally traversing a matrix
+
+
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int mat[][]) {
+        int m = mat.length;
+        int n = mat[0].length;
+        ArrayList<Integer> res = new ArrayList<>();
+        int top = 0, bottom = m-1;
+        int left = 0, right = n-1;
+        while(top <= bottom && left <= right){
+            for(int i = left; i<=right; i++){
+                res.add(mat[top][i]);
+            }
+            top++;
+            
+            for(int i = top; i<=bottom; i++){
+                res.add(mat[i][right]);
+            }
+            right--;
+            
+            if(top <= bottom){
+                for(int i = right; i>= left; --i){
+                    res.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right){
+                for(int i = bottom; i>=top; --i){
+                    res.add(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
