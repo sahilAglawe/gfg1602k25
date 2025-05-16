d922c15dc6345ba3c046bc209eebc5d441c9d1c8
SahilAglawe <sahilaglawe66@gmail.com>
2025-05-16 10:38:45 +0530

    Add implementation for setting matrix zeroes

---
+++ Day41.java
@@ -0,0 +1,40 @@
+// Question : Set Matrix Zeroes
+
+
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int firstCol = 1;
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(mat[i][j] == 0){
+                    mat[i][0] = 0;
+                    if(j != 0){
+                        mat[0][j] = 0;
+                    } else {
+                        firstCol = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(mat[i][0] == 0 || mat[0][j] == 0){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        if(mat[0][0] == 0){
+            for(int j = 0; j<m; j++){
+                mat[0][j] = 0;
+            }
+        }
+        if(firstCol == 0){
+            for(int i = 0; i<n; i++){
+                mat[i][0] = 0;
+            }
+        }
+    }
+}
