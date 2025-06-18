16724c2cc1f00cdec8444f6d4da948517562810a
SahilAglawe <sahilaglawe66@gmail.com>
2025-06-18 10:50:58 +0530

    Add implementation for LRU Cache

---
+++ 09Linked List/Day74.java
@@ -0,0 +1,68 @@
+// Question : LRU Cache Implementation
+
+
+class LRUCache {
+    
+        private int capacity;
+        private Map<Integer, Node> map;
+        private Node head, tail;
+
+  
+        class Node {
+            int key, value;
+            Node prev, next;
+            Node(int key, int value){
+                this.key = key;
+                this.value = value;
+            }
+        }
+        
+    
+
+    public LRUCache(int cap){
+        this.capacity = cap;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key){
+        if(!map.containsKey(key))
+        return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertToHead(node);
+        return node.value;
+    }
+
+        
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            insertToHead(node);
+        } else {
+            if(map.size() == capacity){
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node newNode = new Node(key , value);
+            map.put(key, newNode);
+            insertToHead(newNode);
+        }
+    }
+    private void remove(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    private void insertToHead(Node node){
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+}
