7e5a2a123a3eb94d888c05695ffe6492d5ba0052
SahilAglawe <sahilaglawe66@gmail.com>
2025-06-27 10:49:03 +0530

    Add implementation for level order traversal of a binary tree

---
+++ Tree/Day80.java
@@ -0,0 +1,27 @@
+// Question : Level order traversal
+
+
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 0; i< size; i++){
+                Node node = q.remove();
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+                list.add(node.data);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
+
+// Time Complexity: O(N) where N is the number of nodes in the binary tree.
+// Space Complexity: O(N) for the queue and the list to store the result, where
